26bc6726f57bb8dfcbd9d8b969d8889d738087b5
Vlad Efimenko <efimenko.vv@mail.ru>
2019-06-29 00:22:40 +0300

    dbUser2

--- application/core/DataBase.php
+++ application/core/DataBase.php
@@ -15,7 +15,7 @@ final class DataBase implements DataBaseInt
     private function __construct()
     {
         try{
-            $this->link = new \PDO('mysql:host=127.0.0.1;dbname=u0742966_default', 'u0742966_default', '7!RRuV0Y');
+            $this->link = new \PDO('mysql:host=localhost;dbname=u0742966_default', 'u0742966_default', '7!RRuV0Y');
         }catch(\PDOException $exception){
             //$this->link = new mysql('127.0.0.1', 'root', '', 'mway');
         }
